65f125faded0d9d8d1517390cb610bca803b84d9
ParthDevani <parthdevani2121@gmail.com>
2022-02-22 10:22:23 +0530

    create demo blog app

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  root "articles#index"
+
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id",to: "articles#show"
+  # get "/articles/new",to: "articles#new"
+  # post "/articles",to: "articles#create"
+  # get "/articles/:id/edit",to: "articles#edit"
+  # patch "/articles/:id",to: "articles#update"
+  get "/articles/:id/delete",to: "articles#destroy"
+
+  resources :articles
+
+end
